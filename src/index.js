67d300967a956cbf98a92b96c00f74de5fb5be5b
Dezy9142 <dezykumari8969@gmail.com>
2024-04-02 18:20:46 +0530

    user and video models with jwt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import { app } from "./app.js";
 import DbConnect from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config({path:'./env'})
+
+const port=process.env.PORT || 8000;
+
 DbConnect()
+.then(()=>{
+    app.listen(port,()=>{
+     console.log(`Server is running at port ${port}`)
+    })
+})
+.catch((err)=>console.log(`DB connection failed !!` ,err))
---
+++ src/models/user.model.js
@@ -0,0 +1,103 @@
+import mongoose  from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcryptjs"
+
+const userSchema= new mongoose.Schema(
+{
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true 
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true,
+    },
+    avatar:{
+        // cloudinary url
+        type:String, 
+        required:true
+        
+    },
+    coverImage:{
+        type:String, 
+        
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+    }
+    ,
+    refreshToken:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Video'
+        }
+    ]
+
+},
+{timestamps:true}
+);
+
+// pre middleware doest not take callback function we have write normal function becase 
+// callBack func has no access of this 
+userSchema.pre("save",function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10);
+    next();
+})
+
+// injecting methods on schema for comapring of paswword
+userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+
+// accessToken  and refreshToken method insert in schema both are JWT Token 
+// both are same but diffrence in  usage
+userSchema.methods.generateAccessToken= function(){
+   return Jwt.sign(
+    {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+   )
+}
+userSchema.methods.generateRefreshToken= function(){
+    return Jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+       )
+
+}
+
+
+
+export const User=mongoose.model('User',userSchema)
